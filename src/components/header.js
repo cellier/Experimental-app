92243e93308451cf7cf97eb9cf1e2f3f1b222756
cellier <cellier@mail.com>
2018-07-27 22:33:01 +0800

    first section

--- src/components/header.js
+++ src/components/header.js
@@ -1,32 +1,18 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import './Header.less'
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+  <div className="Header">
+      <div className="HeaderGroup">
+       <Link to="/"> 
+        <img src={require('../images/figma_logo.svg')} alt=""/>
+       </Link>
+       <Link to='/'>Features</Link>
+       <Link to='/'>About</Link>
+       <Link to='/'>Login</Link>
+       <Link to='/'><button>Sign Up</button></Link>
+      </div>
   </div>
 )
 
